ec2356368ccd5bfce70f772e67da62228528dc99
Liam McRoe <liam.mcroe@autorek.com>
2022-12-02 16:57:18 +0000

    Update approach to use dicts instead of switch
    Update approach to use dicts instead of switch

--- AdventOfCode.DayTwo/DayTwo.cs
+++ AdventOfCode.DayTwo/DayTwo.cs
@@ -1,42 +1,49 @@
 ﻿namespace AdventOfCode.DayTwo
 {
-    public static class DayTwo
-    {
-        public static void Run(string inputPath)
-        {
-            Console.WriteLine($"Before guide explanied fully (part one): {GetGameScore(inputPath, false)}");
-            Console.WriteLine($"After guide explanied fully (part two): {GetGameScore(inputPath, true)}");
-        }
+	public static class DayTwo
+	{
+		private static readonly Dictionary<string, int> partOneScores = new()
+		{
+			["A X"] = 4,
+			["A Y"] = 8,
+			["A Z"] = 3,
+			["B X"] = 1,
+			["B Y"] = 5,
+			["B Z"] = 9,
+			["C X"] = 7,
+			["C Y"] = 2,
+			["C Z"] = 6,
+		};
 
-        private static int GetGameScore(string inputPath, bool guideExplained)
-        {
-            using var sr = File.OpenText(inputPath);
-            string? line;
-            int currentScore = 0;
-            while ((line = sr.ReadLine()) != null)
-            {
-                currentScore += GetRoundScore(line, guideExplained);
-            }
-            return currentScore;
-        }
+		private static readonly Dictionary<string, int> partTwoScores = new()
+		{
+			["A X"] = 3,
+			["A Y"] = 4,
+			["A Z"] = 8,
+			["B X"] = 1,
+			["B Y"] = 5,
+			["B Z"] = 9,
+			["C X"] = 2,
+			["C Y"] = 6,
+			["C Z"] = 7,
+		};
 
-        private static int GetRoundScore(string roundDef, bool guideExplained)
-        {
-			(char playerOne, char playerTwo) round = (roundDef[0], roundDef[2]);
+		public static void Run(string inputPath)
+		{
+			Console.WriteLine($"Before guide explanied fully (part one): {GetGameScore(inputPath, false)}");
+			Console.WriteLine($"After guide explanied fully (part two): {GetGameScore(inputPath, true)}");
+		}
 
-            return round switch
+		private static int GetGameScore(string inputPath, bool guideExplained)
+		{
+			using var sr = File.OpenText(inputPath);
+			string? line;
+			int currentScore = 0;
+			while ((line = sr.ReadLine()) != null)
 			{
-				('A', 'X') => guideExplained ? 3 : 4,
-				('A', 'Y') => guideExplained ? 4 : 8,
-				('A', 'Z') => guideExplained ? 8 : 3,
-				('B', 'X') => guideExplained ? 1 : 1,
-				('B', 'Y') => guideExplained ? 5 : 5,
-				('B', 'Z') => guideExplained ? 9 : 9,
-				('C', 'X') => guideExplained ? 2 : 7,
-				('C', 'Y') => guideExplained ? 6 : 2,
-				('C', 'Z') => guideExplained ? 7 : 6,
-				_ => throw new NotImplementedException()
-			};
-        }
+				currentScore += guideExplained ? partTwoScores[line] : partOneScores[line];
+			}
+			return currentScore;
+		}
 	}
 }
