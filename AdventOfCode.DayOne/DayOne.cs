9a65d0c99f2b6f324ed43dff12bcae75578a0583
Liam McRoe <liam.mcroe@autorek.com>
2022-12-02 15:02:29 +0000

    Add day one and two solutions
    Add day one and two solutions

---
+++ AdventOfCode.DayOne/DayOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.DayOne
+{
+    public static class DayOne
+    {
+        public static void Run(string inputPath)
+        {
+            var caloriesByElf = GetSortedCalories(inputPath);
+            Console.WriteLine($"Most cals held by an elf: {caloriesByElf[0]}");
+            Console.WriteLine($"Total cals held by top 3 elves: {caloriesByElf.Take(3).Sum()}");
+        }
+
+        private static List<int> GetSortedCalories(string inputPath)
+        {
+            List<int> caloriesByElf = new List<int>();
+            using var sr = File.OpenText(inputPath);
+            string? line;
+            int currentElfTotal = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    caloriesByElf.Add(currentElfTotal);
+                    currentElfTotal = 0;
+                }
+                else
+                {
+                    currentElfTotal += int.Parse(line);
+                }
+            }
+            caloriesByElf.Add(currentElfTotal); // Ensure final elf is counted.
+            caloriesByElf.Sort((a, b) => b.CompareTo(a));
+            return caloriesByElf;
+        }
+    }
+}
---
+++ AdventOfCode.DayTwo/DayTwo.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode.DayTwo
+{
+    public static class DayTwo
+    {
+        public static void Run(string inputPath)
+        {
+            Console.WriteLine($"Before guide explanied fully (part one): {GetGameScore(inputPath, false)}");
+            Console.WriteLine($"After guide explanied fully (part two): {GetGameScore(inputPath, true)}");
+        }
+
+        private static int GetGameScore(string inputPath, bool guideExplained)
+        {
+            using var sr = File.OpenText(inputPath);
+            string? line;
+            int currentScore = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                currentScore += GetRoundScore(line, guideExplained);
+            }
+            return currentScore;
+        }
+
+        private static int GetRoundScore(string roundDef, bool guideExplained)
+        {
+			(char playerOne, char playerTwo) round = (roundDef[0], roundDef[2]);
+
+            return round switch
+			{
+				('A', 'X') => guideExplained ? 3 : 4,
+				('A', 'Y') => guideExplained ? 4 : 8,
+				('A', 'Z') => guideExplained ? 8 : 3,
+				('B', 'X') => guideExplained ? 1 : 1,
+				('B', 'Y') => guideExplained ? 5 : 5,
+				('B', 'Z') => guideExplained ? 9 : 9,
+				('C', 'X') => guideExplained ? 2 : 7,
+				('C', 'Y') => guideExplained ? 6 : 2,
+				('C', 'Z') => guideExplained ? 7 : 6,
+				_ => throw new NotImplementedException()
+			};
+        }
+	}
+}
