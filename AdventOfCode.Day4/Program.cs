70ac9cb789866b16c2435ff3144a63daab82858a
Liam McRoe <liam.mcroe@autorek.com>
2022-12-04 22:22:26 +0000

    Remove duplicated word in part 2 string
    Remove duplicated word in part 2 string

--- AdventOfCode.Day4/Program.cs
+++ AdventOfCode.Day4/Program.cs
@@ -9,9 +9,8 @@ var part1 = File.ReadAllLines("Input.txt")
 
 var part2 = File.ReadAllLines("Input.txt")
 	.Select(x => x.Split(',').Select(y => y.Split('-').Select(z => int.Parse(z)).ToArray()).ToArray())
-	.Where(x =>
-		(x[0][0] <= x[1][1] && x[1][0] <= x[0][1]))
+	.Where(x => (x[0][0] <= x[1][1] && x[1][0] <= x[0][1]))
 	.Count();
 
 Console.WriteLine($"Part one (ranges fully contained in partners'): {part1}");
-Console.WriteLine($"Part two (ranges ranges where there is overlap with partners'): {part2}");
+Console.WriteLine($"Part two (ranges where there is overlap with partners'): {part2}");
