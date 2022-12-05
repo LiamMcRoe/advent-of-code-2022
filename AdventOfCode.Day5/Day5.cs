28421aad3d1db934b29b98d16a1718044583df28
Liam McRoe <liam.mcroe@autorek.com>
2022-12-05 18:25:31 +0000

    Add solution for day 5
    Add solution for day 5 puzzle, including generic parser

---
+++ AdventOfCode.Day5/Day5.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Day5
+{
+	public static class Day5
+	{
+		public static void Run(string inputPath)
+		{
+			Console.WriteLine($"Part one final positions: {GetFinalPositions(inputPath, false)}");
+			Console.WriteLine($"Part two final positions: {GetFinalPositions(inputPath, true)}");
+		}
+
+		private static string GetFinalPositions(string inputPath, bool moveMultiple)
+		{
+			var parser = new InputParser(File.ReadAllLines(inputPath));
+			var crates = parser.GetCrateStacks();
+			SortCrates(crates, parser.GetInstructions(), moveMultiple);
+			var sb = new StringBuilder();
+			foreach (var crateStack in crates) sb.Append(crateStack.TryPeek(out var crate) ? crate : ' ');
+			return sb.ToString();
+		}
+
+		private static void SortCrates(List<Stack<char>> crates, List<(int NumberToMove, int MoveFromIndex, int MoveToIndex)> instructions, bool moveMultiple) 
+		{
+			foreach (var instruction in instructions)
+			{
+				ExecuteInstruction(crates, instruction, moveMultiple);
+			}
+		}
+
+		private static void ExecuteInstruction(List<Stack<char>> crates, (int NumberToMove, int MoveFromIndex, int MoveToIndex) instruction, bool moveMultiple)
+		{
+			var leftToMove = instruction.NumberToMove;
+			var moveAtOnce = moveMultiple ? instruction.NumberToMove : 1;
+			while (leftToMove != 0)
+			{
+				var cratesToMove = new Stack<char>();
+				while (cratesToMove.Count < moveAtOnce)
+				{
+					cratesToMove.Push(crates[instruction.MoveFromIndex].Pop());
+				}
+				foreach (var crate in cratesToMove) crates[instruction.MoveToIndex].Push(crate);
+
+				leftToMove -= moveAtOnce;
+			}
+		}
+	}
+}
---
+++ AdventOfCode.Day5/InputParser.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Day5
+{
+	public class InputParser
+	{
+		private readonly string[] cratePositions;
+		private readonly string[] instructions;
+
+		public InputParser(string[] input)
+		{
+			var divideIndex = Array.IndexOf(input, string.Empty);
+			this.cratePositions = input[..(divideIndex - 1)];
+			this.instructions = input[(divideIndex + 1)..];
+			foreach(var c in cratePositions) Console.WriteLine(c);
+		}
+
+		public List<Stack<char>> GetCrateStacks()
+		{
+			var stacks = new List<Stack<char>>();
+			for (int i = cratePositions.Length - 1; i >= 0; i--)
+			{ 
+				var parsedLine = ParseCrateLine(cratePositions[i]);
+				for (int j = 0; j <= parsedLine.Length - 1; j++) 
+				{
+					if (stacks.Count <= j) stacks.Add(new Stack<char>());
+					if (parsedLine[j] != ' ') stacks[j].Push(parsedLine[j]);
+				}
+			}
+			return stacks;
+		}
+
+		public List<(int NumberToMove, int MoveFromIndex, int MoveToIndex)> GetInstructions()
+		{
+			var parsedInstructions = new List<(int NumberToMove, int MoveFromIndex, int MoveToIndex)>();
+			foreach (var instruction in instructions)
+			{
+				var splitInstruction = instruction.Split(' ');
+				parsedInstructions.Add((int.Parse(splitInstruction[1]), int.Parse(splitInstruction[3]) - 1, int.Parse(splitInstruction[5]) - 1));
+			}
+			return parsedInstructions;
+		}
+
+		private string ParseCrateLine(string crateLine)
+		{
+			var parsedLine = new StringBuilder();
+			for (int i = 1; i <= crateLine.Length - 1; i += 4) parsedLine.Append(crateLine[i]);
+			return parsedLine.ToString();
+		}
+	}
+}
