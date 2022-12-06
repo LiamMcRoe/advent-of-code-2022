43da692e6b647faf204d1001f83fb4da3e0b6a95
Liam McRoe <liam.mcroe@autorek.com>
2022-12-06 09:48:38 +0000

    Add day 6 solution
    Add day 6 solution. 1 line for both parts!

---
+++ AdventOfCode.Day6/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Func<int, int> getResult = chunkSize => new[] { File.ReadAllText("Input.txt") }
+	.Select(s =>
+		Enumerable.Range(0, s.Length - (chunkSize + 1))
+		.Select(i => (i + chunkSize, s.Substring(i, chunkSize)))
+		.First(x => x.Item2.Distinct().Count() == chunkSize)
+		.Item1)
+	.First();
+
+Console.WriteLine($"Part one: {getResult(4)}");
+Console.WriteLine($"Part two: {getResult(14)}");
