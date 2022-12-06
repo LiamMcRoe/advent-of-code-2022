a15c4470d0e3e1a06c019de381f5031fe1abd3e6
Liam McRoe <liam.mcroe@autorek.com>
2022-12-06 09:54:20 +0000

    Rename func to more descriptive name
    Rename func to more descriptive name

--- AdventOfCode.Day6/Program.cs
+++ AdventOfCode.Day6/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-Func<int, int> getResult = chunkSize => new[] { File.ReadAllText("Input.txt") }
+Func<int, int> getFirstDistinctChunk = chunkSize => new[] { File.ReadAllText("Input.txt") }
 	.Select(s =>
 		Enumerable.Range(0, s.Length - (chunkSize + 1))
 		.Select(i => (i + chunkSize, s.Substring(i, chunkSize)))
@@ -7,5 +7,5 @@ Func<int, int> getResult = chunkSize => new[] { File.ReadAllText("Input.txt") }
 		.Item1)
 	.First();
 
-Console.WriteLine($"Part one: {getResult(4)}");
-Console.WriteLine($"Part two: {getResult(14)}");
+Console.WriteLine($"Part one: {getFirstDistinctChunk(4)}");
+Console.WriteLine($"Part two: {getFirstDistinctChunk(14)}");
