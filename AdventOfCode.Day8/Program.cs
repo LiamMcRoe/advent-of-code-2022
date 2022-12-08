5ac2e8bc39756845e01303aae39c95a79b3a603a
Liam McRoe <liammcroe1995@gmail.com>
2022-12-08 21:44:55 +0000

    Add day 8 part 1 solution

---
+++ AdventOfCode.Day8/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = File.ReadAllLines("Input.txt");
+
+var part1 = input
+    .Select((x, i) =>
+        x.Where((y, j) =>
+            i == 0 ||
+            i == input.Length - 1 ||
+            j == 0 ||
+            j == input[i].Length - 1 ||
+            input[..i].Select(x => x[j]).All(k => !(k >= y)) ||
+            input[(i + 1)..].Select(x => x[j]).All(k => !(k >= y)) ||
+            input[i][..j].All(k => !(k >= y)) ||
+            input[i][(j + 1)..].All(k => !(k >= y)))
+        .Count())
+    .Sum();
+
+Console.WriteLine($"Part one: {part1}");
