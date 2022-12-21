ba25d4012f337c462b764319bf3e6debece014e8
Liam McRoe <liam.mcroe@autorek.com>
2022-12-21 17:23:22 +0000

    Add day 20 part 2 and refactor

--- AdventOfCode.Day20/Day20.cs
+++ AdventOfCode.Day20/Day20.cs
@@ -1,26 +1,53 @@
-﻿namespace AdventOfCode.Day20
+﻿using System.Diagnostics;
+
+namespace AdventOfCode.Day20
 {
 	public static class Day20
 	{
 		public static void Run(string inputPath)
 		{
-			var input = File.ReadAllLines(inputPath).Select((x, i) => new FileElement(i, 811589153 * long.Parse(x))).ToList();
-			var toProcess = File.ReadAllLines(inputPath).Select((x,i) => new FileElement(i, 811589153 * long.Parse(x))).ToList();
-			var numberElements = input.Count;
-			var processedElements = new HashSet<FileElement>();
-			for (int i = 0; i < 10; i++)
+			var input = File.ReadAllLines(inputPath);
+			PartOne(input);
+			PartTwo(input);
+		}
+
+		private static void PartOne(string[] input)
+		{
+			var fileToMix = input.Select((x, i) => new FileElement(i, long.Parse(x))).ToList();
+			Mix(fileToMix, 1);
+			var coord = GetCoordinates(fileToMix);
+			Console.WriteLine($"Co-ords without key (part one): {coord}");
+		}
+
+		private static void PartTwo(string[] input)
+		{
+			var fileToMix = input.Select((x, i) => new FileElement(i, 811589153 * long.Parse(x))).ToList();
+			Mix(fileToMix, 10);
+			var coord = GetCoordinates(fileToMix);
+			Console.WriteLine($"Co-ords with key (part two): {coord}");
+		}
+
+		private static void Mix(List<FileElement> fileToMix, int timesToMix)
+		{
+			var timesMixed = 0;
+			while (timesMixed < timesToMix)
 			{
-				Console.WriteLine($"i = {i}");
-				foreach (var element in toProcess)
+				for (int i = 0; i < fileToMix.Count; i++)
 				{
-					var currentIndex = input.IndexOf(element);
-					var item = input[currentIndex];
-					if (item.Value == 0) continue;
-					input.MoveForward(currentIndex, item.Value);
+					var currentElement = fileToMix.First(x => x.OriginalPosition == i);
+					var currentIndex = fileToMix.IndexOf(currentElement);
+
+					if (currentElement.Value == 0) continue;
+					fileToMix.MoveForward(currentIndex, currentElement.Value % (fileToMix.Count - 1));
 				}
+				timesMixed++;
 			}
-			var zeroIndex = input.IndexOf(input.First(x => x.Value == 0));
-			Console.WriteLine(input[(zeroIndex + 1000) % numberElements].Value + input[(zeroIndex + 2000) % numberElements].Value + input[(zeroIndex + 3000) % numberElements].Value);
+		}
+
+		private static long GetCoordinates(List<FileElement> file)
+		{
+			var zeroIndex = file.IndexOf(file.First(x => x.Value == 0));
+			return file[(zeroIndex + 1000) % file.Count].Value + file[(zeroIndex + 2000) % file.Count].Value + file[(zeroIndex + 3000) % file.Count].Value;
 		}
 	}
 }
